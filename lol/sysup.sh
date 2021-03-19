b7209b67bc3966369fa23b57c9bb11e8dce9b81b
The Otherside <80984256+THEOTHERSIDESEC@users.noreply.github.com>
2021-03-20 01:41:56 +0200

    Add files via upload

---
+++ lol/sysup.sh
@@ -0,0 +1,30 @@
+cyn='\e[0;36m'
+
+lgrn='\e[1;32m'
+
+r='\e[1;31m'
+
+y='\e[1;33m'
+################
+clear
+echo
+echo
+echo
+echo ' 
+ 
+      
+                     █▄░▒█ █▀▀█ ▀▀█▀▀ █▀▀
+                    █▒█▒█ █░░█ ░░█░░ █▀▀
+                    █░░▀█ ▀▀▀▀ ░░▀░░ ▀▀▀' | lolcat
+echo " "
+echo " "
+cat letter.txt
+echo " "
+echo "                      " Be responsible with the virus| lolcat
+echo " " 
+echo " "
+sleep 50.0
+clear 
+cd $HOME/Virus
+bash lc-virus.sh
+exit 
